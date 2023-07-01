ad61512556c701052247217ecdbc85d37746c3b7
arthur1rt <shalkioth@gmail.com>
2023-07-01 11:59:48 -0700

    fix?

--- iFrameLoader.js
+++ iFrameLoader.js
@@ -1,4 +1,4 @@
-let videoPlayer;
+videoPlayer = null;
 function onYouTubeIframeAPIReady() {
     // Note: This function must be in the global scope to be seen by the API
     // You need a video ID, not the whole URL, so we'll have to extract that later
