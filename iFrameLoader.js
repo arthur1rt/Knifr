3dd04ab0d8dba643fec9894b314f1aa58664c5e0
arthur1rt <shalkioth@gmail.com>
2023-07-01 11:56:56 -0700

    fix?

---
+++ iFrameLoader.js
@@ -0,0 +1,10 @@
+let videoPlayer;
+function onYouTubeIframeAPIReady() {
+    // Note: This function must be in the global scope to be seen by the API
+    // You need a video ID, not the whole URL, so we'll have to extract that later
+    videoPlayer = new YT.Player('VideoPreview', {
+        height: '540',
+        width: '960',
+        videoId: '', // initially empty
+    });
+}
--- script.js
+++ script.js
@@ -10,17 +10,6 @@ function removeBracketedSuffix(str) {
     return str;
 }
 
-let videoPlayer;
-function onYouTubeIframeAPIReady() {
-    // Note: This function must be in the global scope to be seen by the API
-    // You need a video ID, not the whole URL, so we'll have to extract that later
-    videoPlayer = new YT.Player('VideoPreview', {
-        height: '540',
-        width: '960',
-        videoId: '', // initially empty
-    });
-}
-
 // Define a mapping of accented characters to their unaccented equivalents
 const accentMap = {
     'á': 'a',
